206459005db6a2089ab8da68c3072ec68f83baf9
Q-yuannn <3355242951@qq.com>
2025-04-29 19:46:18 +0800

    Create attributes for the tuplespace class

--- sever.py
+++ sever.py
@@ -1,6 +1,11 @@
 class Tuple_space:
     def __init__(self, *args):
-        self.tuples = []
+        self.tuples = {}
+        self.client_connected_count = 0
+        self.totaloperations_count = 0
+        self.read_count = 0
+        self.get_count = 0
+        self.put_count = 0
 
 
     
